d94e2f614f3ea2b8a11235e64f2308b1b22910c3
nutristar <deniszmail@gmail.com>
2022-09-29 20:45:04 +0200

    snipers - abrev+tab

---
+++ how_to_rise__function.js
@@ -0,0 +1,52 @@
+function testCase(a) {
+    var x;
+    switch (a) {
+        case 0:
+            x = "Ноль";
+            break;
+        case 1:                               // если a равна единице то записываем
+            x = "Один";  // в переменную x фразу "Переменная a равна единице!"
+            break;
+        case 2:
+            x = "Два";
+            break;
+        case 3:
+            x = "Три!";
+            break;
+        case 4:
+            x = "Четыре";
+            break;
+        case 5:
+            x = "Пять";
+            break;
+        case 6:
+            x = "Шесть";
+            break;
+        case 7:
+            x = "Семь";
+            break;
+        case 8:
+            x = "Восемь";
+            break;
+        case 9:
+            x = "Девять";
+            break;
+
+    }
+    // Тут нужно написать решение
+    return x;
+}
+
+// и второй способ
+
+const x=(radius)=>{return 3.14*radius**2};
+const s = x(2);
+// const .
+console.log(s)
+document.write(s)
+
+
+
+export default HowToRiseFunction;
+export default HowToRiseFunction;
+
