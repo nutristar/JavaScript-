b518196691b0aa796f795c8e6fd6120dad89f720
nutristar <deniszmail@gmail.com>
2022-10-03 02:07:04 +0200

    str lesson

--- factorial.js
+++ factorial.js
@@ -1,10 +1,25 @@
-
-const faktorial=(a)=> {
-    if (a===1) {
-        return a;
-    } else {
-        return a*faktorial(a-1);
+//
+// const faktorial=(a)=> {
+//     if (a===1) {
+//         return a;
+//     } else {
+//         return a*faktorial(a-1);
+//     }
+// }
+const faktorial=(num)=>{
+    let result =1
+    let counter =1
+    while (counter <= num) {
+        result = result * counter;
+        counter = counter + 1;
+        console.log(result);
     }
-}
-
+    return result;
+};
 document.write(faktorial(4))
+// console.log(faktorial(4))
+// alert(faktorial(a))
+
+
+
+
---
+++ str cicles tests/strs.js
@@ -0,0 +1,5 @@
+function reverse(str){
+    return str.split("").reverse().join("");
+}
+
+document.write((reverse('hello, world!')))
